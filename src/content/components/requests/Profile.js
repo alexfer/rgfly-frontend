bc910899bbd703b83bac1c08960532a6bacd73c4
Alexander S <autoportal@email.ua>
2024-02-27 17:53:49 +0200

    Update Profile.js

--- src/content/components/requests/Profile.js
+++ src/content/components/requests/Profile.js
@@ -4,17 +4,28 @@ import {AES, enc} from "crypto-js";
 const getToken = () => {
     let bytes;
     const token = localStorage.getItem('token');
+
     if (token !== null) {
         bytes = AES.decrypt(token, process.env.REACT_APP_AUTH_SECRET);
-        return bytes.toString(enc.Utf8);
+        return 'Bearer ' + bytes.toString(enc.Utf8);
     }
 }
 
+const requestOptions = {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': getToken()
+    },
+};
+
 const Profile = (props) => {
+
     const id = props.id;
     const url = 'https://dev.moniheal.com/api/profile/' + id;
     const [data, setData] = useState([]);
-    fetch(url)
+
+    fetch(url, requestOptions)
         .then(async (response) => {
             const isJson = response.headers.get('content-type')?.includes('application/json');
             const data = isJson && await response.json();
