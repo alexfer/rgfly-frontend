0f98dc71bf82f17c276321d5a400fc5130a5f5b7
Alexander S <autoportal@email.ua>
2024-02-28 16:35:20 +0200

    Update User.js

--- src/components/requests/User.js
+++ src/components/requests/User.js
@@ -1,20 +1,10 @@
-import {AES, enc} from "crypto-js";
-
-const getToken = () => {
-    let bytes;
-    const token = localStorage.getItem('token');
-
-    if (token !== null) {
-        bytes = AES.decrypt(token, process.env.REACT_APP_AUTH_SECRET);
-        return 'Bearer ' + bytes.toString(enc.Utf8);
-    }
-}
+import getToken from '../utils/token'
 
 const requestOptions = {
     method: 'GET',
     headers: {
         'Content-Type': 'application/json',
-        'Authorization': getToken()
+        'Authorization': getToken
     },
 };
 
